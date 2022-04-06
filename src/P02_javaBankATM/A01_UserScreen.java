dda3731235f61e09d70b54ec3818a4720a0c62ac
Asim Muhsin Basaran <asmmuhsinbasaran93@gmail.com>
2022-04-06 23:57:38 +0300

    06.04.2022

---
+++ src/P02_javaBankATM/A01_UserScreen.java
@@ -0,0 +1,12 @@
+package P02_javaBankATM;
+import static P02_javaBankATM.A02_InfoControl.atmStart;
+
+public class A01_UserScreen {
+
+    public static void main(String[] args) {
+        System.out.println("*************************************");
+        System.out.println("******** Welcome to JavaBank ********");
+        atmStart();
+
+    }
+}
